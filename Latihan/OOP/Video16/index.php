757322cf8159c8af1194702b731ceab96b807389
Irsan-02 <irsan.taufik38@gmail.com>
2024-10-14 13:56:41 +0700

    Rename Folder

--- Latihan/OOP/Video16/index.php
+++ Latihan/OOP/Video16/index.php
@@ -1,77 +1,15 @@
 <?php
-abstract class Produk
-{
-    protected $judul,
-        $penulis,
-        $penerbit,
-        $harga,
-        $diskon = 0;
 
+require_once 'App/init.php';
 
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
 
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
-    {
-        $this->judul = $judul;
-        $this->penulis = $penulis;
-        $this->penerbit = $penerbit;
-        $this->harga = $harga;
-    }
+$cetakProduk = new cetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
 
-    public function setJudul($judul)
-    {
-        $this->judul = $judul;
-    }
+echo "<hr>";
 
-    public function getJudul()
-    {
-        return $this->judul;
-    }
-
-    public function setPenulis($penulis)
-    {
-        $this->penulis = $penulis;
-    }
-
-    public function getPenulis()
-    {
-        return $this->penulis;
-    }
-
-    public function setPenerbit($penerbit)
-    {
-        $this->penerbit = $penerbit;
-    }
-
-    public function getPenerbit()
-    {
-        return $this->penerbit;
-    }
-
-    public function setDiskon($diskon)
-    {
-        $this->diskon = $diskon;
-    }
-
-    public function getDiskon()
-    {
-        return $this->diskon;
-    }
-
-    public function setHarga($harga)
-    {
-        $this->harga = $harga;
-    }
-
-    public function getHarga()
-    {
-        return $this->harga - ($this->harga * $this->diskon / 100);
-    }
-
-
-    public function getLabel()
-    {
-        return " $this->penulis, $this->penerbit";
-    }
-
-    abstract function getInfo();
-}
+new Coba();
